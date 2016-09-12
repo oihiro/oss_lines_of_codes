eb95fc1b90775697836d8839dd1d68f984eee21f
OI Hirokazu <oi.hirokazu@gmail.com>
2016-09-13 05:56:35 +0900

    added insert_jpegsr6.sql. added the data existance check with psql into get_lines.sh

---
+++ insert_jpegsr6.sql
@@ -0,0 +1,3 @@
+insert into t_oss_lines(oss_name, version, download_url, lines_of_codes) values
+('libjpeg','6b','https://sourceforge.net/projects/libjpeg/files/libjpeg/6b/jpegsr6.zip/download','18631');
+
